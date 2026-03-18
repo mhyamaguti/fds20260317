5c070690b89876b96147a020ee225f73f55aacf5
aliceguzas <alice.guzas@edu.pucrs.br>
2026-03-17 22:03:40 -0300

    Update App.java

--- presenca20250317/src/App.java
+++ presenca20250317/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("FUNDAMENTOS DE DESENVOLVIMENTO DE SOFTWARE");
         System.out.println("Presenca da aula 17/3/2026: ");
+        System.out.println("Alice Guzas");
         
         // Insira uma linha que mostre o seu nome na tela    
         // Iohanna Sofia Dietz Maldaner :)
